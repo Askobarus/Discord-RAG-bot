ca572aaab28f7940282f67c5374a7510adfdd2b5
Askobarus <salnikov.salnikov2010@yandex.ru>
2026-08-19 14:44:07 +0300

    Add ChunkModel
    Add db table model for message history chunks to use in RAG pipeline in future + sqlite_vec support

--- database.py
+++ database.py
@@ -1,17 +1,28 @@
+import sqlite_vec
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 from sqlalchemy import text
+from models.messages import Base, MessageModel, ChunkModel
+
 
 DATABASE_URL = "sqlite+aiosqlite:///messages.db"
 engine = create_async_engine(DATABASE_URL, echo=False)
 new_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
-class Base(DeclarativeBase, MappedAsDataclass):
-    pass
+# 1. Автоматически загружаем расширение при каждом подключении
+@event.listens_for(engine.sync_engine, "connect")
+def load_sqlite_vec_extension(dbapi_conn, connection_record):
+    # dbapi_conn здесь является объектом aiosqlite.Connection
+    dbapi_conn.enable_load_extension(True)
+    sqlite_vec.load(dbapi_conn)
+
+
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+        
         await conn.execute(text("PRAGMA journal_mode=WAL;"))
         await conn.execute(text("PRAGMA synchronous=NORMAL;"))
         await conn.execute(text("PRAGMA cache_size=-32000;"))
--- models/messages.py
+++ models/messages.py
@@ -1,7 +1,10 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Index
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
+from sqlalchemy import Index, String, LargeBinary
 from datetime import datetime
-from database import Base
+
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
 
 class MessageModel(Base):
     __tablename__ = 'messages'
@@ -11,3 +14,12 @@ class MessageModel(Base):
     author: Mapped[str]
     content: Mapped[str]
     timestamp: Mapped[datetime]
+       
+class ChunkModel(Base):
+    __tablename__ = 'chunks'
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    channel_id: Mapped[str] = mapped_column(String(50))
+    message_ids: Mapped[str] = mapped_column(String(500)) # "123,124,125"
+    content: Mapped[str] = mapped_column(String(4000))    # Склеенный текст диалога
+    embedding: Mapped[bytes] = mapped_column(LargeBinary) # Вектор как BLOB
